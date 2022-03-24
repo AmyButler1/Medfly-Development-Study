bc80a1e71ff4e78ec86bfa3522561c28496c6c57
AmyButler1 <mmy19jtu@uea.ac.uk>
2022-03-24 09:45:47 +0000

    updated data files

--- scripts/pilot_egg_size_both_dates.R
+++ scripts/pilot_egg_size_both_dates.R
@@ -2,34 +2,28 @@ source("scripts/functions.R") #loads required functions
 
 ### TIDY DATA #######################################################
 
-egg_size <- read_csv("data/egg size 17_02.csv")
+egg_size2 <- read_csv("data/egg size both.csv")
 # view(egg_size)
 # skim(egg_size)
-head(egg_size)
+head(egg_size2)
 
 
-egg_size <- egg_size %>%
+egg_size2 <- egg_size2 %>%
   rename(fly_line = "line",
-         selection_diet = "line_type") %>% 
-
-
-### PLOTS ############################################################
-
-egg_plots <- function( data = egg_size, egg_measure, line){
-  ggplot(aes(y= .data[[egg_measure]], x= .data[[line]]), data = data)+
-    geom_boxplot(width=0.1, aes(fill= .data[[line]]))+
-    scale_color_brewer(palette = "Set2")+
-    scale_fill_brewer(palette = "Set2")+
-    theme_classic()
-}
-
-
-egg_plots(data = egg_size, egg_measure = "egg_length", line = "fly_line")
-egg_plots(data = egg_size, egg_measure = "egg_width", line = "fly_line")
+         selection_diet = "line_type")
   
+  
+  ### PLOTS ############################################################
+
+egg_plots2 <- function( data = egg_size2, egg_measure, line){ggplot(aes(y= .data[[egg_measure]], x= .data[[line]]), data = data)+ geom_boxplot(width=0.1, aes(fill= .data[[line]]))+ scale_color_brewer(palette = "Set2")+scale_fill_brewer(palette = "Set2")+theme_classic()}
+
+
+egg_plots2(data = egg_size, egg_measure = "egg_length", line = "fly_line")
+egg_plots2(data = egg_size, egg_measure = "egg_width", line = "fly_line")
+
 
 #egg length box plot
-egg_size %>%
+egg_size2 %>%
   ggplot(aes(y= egg_length, x= fly_line))+
   geom_boxplot(width=0.1, aes(fill= fly_line))+
   scale_color_brewer(palette = "Set2")+
@@ -38,7 +32,7 @@ egg_size %>%
   theme_classic()
 
 #egg width box plot
-egg_size %>%
+egg_size2 %>%
   ggplot(aes(y= egg_width, x= fly_line))+
   geom_boxplot(width=0.1, aes(fill= fly_line))+
   scale_color_brewer(palette = "Set2")+
@@ -47,7 +41,7 @@ egg_size %>%
   theme_classic()
 
 #egg volume box plot
-egg_size %>%
+egg_size2 %>%
   ggplot(aes(y= egg_volume, x= fly_line))+
   geom_boxplot(width=0.1, aes(fill= fly_line))+
   scale_color_brewer(palette = "Set2")+
@@ -56,7 +50,7 @@ egg_size %>%
   theme_classic()
 
 #egg volume box plot
-egg_size %>%
+egg_size2 %>%
   ggplot(aes(y= egg_volume, x= selection_diet))+
   geom_boxplot(width=0.1, aes(fill= selection_diet))+
   scale_color_brewer(palette = "Set2")+
@@ -64,14 +58,14 @@ egg_size %>%
   labs(x="Fly line", y="Egg volume (cubic micrometers)")+
   theme_classic()
 
-ggplot(aes( x = egg_length, y= egg_width, colour=selection_diet), data = egg_size)+geom_point()+geom_smooth(method="lm")
+ggplot(aes( x = egg_length, y= egg_width, colour=selection_diet), data = egg_size2)+geom_point()+geom_smooth(method="lm")
 
 ### STATISTICS ##############################
 
 #Spearmans rank correlation coefficient for length and width
-spearmans<- cor.test(egg_size$egg_length, egg_size$egg_width, method="spearman")
+spearmans<- cor.test(egg_size2$egg_length, egg_size2$egg_width, method="spearman")
 spearmans
-##p-value < 2.2e-16 so significant correlation between length and width
+##p-value .... so significant correlation between length and width
 
 #t-test to check volume between line types
 
