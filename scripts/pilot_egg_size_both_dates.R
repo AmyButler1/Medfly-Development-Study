39c3733fe98e95f96684e3f735be805aa4c8ea38
AmyButler1 <mmy19jtu@uea.ac.uk>
2022-04-20 00:16:14 +0100

    spreadsheet updates

--- scripts/pilot_egg_size_both_dates.R
+++ scripts/pilot_egg_size_both_dates.R
@@ -2,7 +2,7 @@ source("scripts/functions.R") #loads required functions
 
 ### TIDY DATA #######################################################
 
-egg_size2 <- read_csv("data/egg size both.csv")
+egg_size2 <- read_excel("data/pilot_data/pilot_egg_size.xlsx")
 # view(egg_size)
 # skim(egg_size)
 head(egg_size2)
@@ -18,8 +18,8 @@ egg_size2 <- egg_size2 %>%
 egg_plots2 <- function( data = egg_size2, egg_measure, line){ggplot(aes(y= .data[[egg_measure]], x= .data[[line]]), data = data)+ geom_boxplot(width=0.1, aes(fill= .data[[line]]))+ scale_color_brewer(palette = "Set2")+scale_fill_brewer(palette = "Set2")+theme_classic()}
 
 
-egg_plots2(data = egg_size, egg_measure = "egg_length", line = "fly_line")
-egg_plots2(data = egg_size, egg_measure = "egg_width", line = "fly_line")
+egg_plots2(data = egg_size2, egg_measure = "egg_length", line = "fly_line")
+egg_plots2(data = egg_size2, egg_measure = "egg_width", line = "fly_line")
 
 
 #egg length box plot
@@ -71,12 +71,12 @@ spearmans
 
 # Rename replicate lines for analysis
 
-egg_ls1 <- lm(egg_volume ~ selection_diet, data = egg_size)
+egg_ls1 <- lm(egg_volume ~ selection_diet, data = egg_size2)
 summary(egg_ls1)
 
-egg_ls2 <- lm(egg_volume ~ selection_diet*fly_line, data = egg_size)
+egg_ls2 <- lm(egg_volume ~ selection_diet*fly_line, data = egg_size2)
 summary(egg_ls2)
 
-egg_ls3 <- lm(egg_volume ~ selection_diet/fly_line, data = egg_size)
+egg_ls3 <- lm(egg_volume ~ selection_diet/fly_line, data = egg_size2)
 summary(egg_ls3)
 plot(egg_ls3)
